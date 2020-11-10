710c9492f4268ab59c71ff470b6471cec30a5987
atat97 <a_tatusko@wp.pl>
2020-11-11 00:05:56 +0100

    Added ability to manage the state of a single bottle of gas -> Editing the value of CO2 bottle from the app in the database.

---
+++ app/src/main/java/com/example/manager/ChangeValuePopUp.java
@@ -0,0 +1,68 @@
+package com.example.manager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ChangeValuePopUp extends Activity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference();
+    DatabaseReference gases_ref = reference.child("Gases");
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changevaluepopup);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        getWindow().setLayout((int) (width*0.8),(int) (height*0.4));
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        TextView textView_seekBar = findViewById(R.id.textView_seekBar);
+        Button button_change_value = findViewById(R.id.button_change_value);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView_seekBar.setText("" + progress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        button_change_value.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Gets child id from previous activity
+                gases_ref.child("0").child("value").setValue(textView_seekBar.getText());
+                finish();
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/manager/GasesListActivity.java
+++ app/src/main/java/com/example/manager/GasesListActivity.java
@@ -3,44 +3,87 @@ package com.example.manager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
+import java.util.Dictionary;
 import java.util.List;
 
 public class GasesListActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference gases = database.getReference().child("Gases").child("NO2");
+    DatabaseReference reference = database.getReference();
+    DatabaseReference gases_ref = reference.child("Gases");
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //TODO: Jedno z dwóch:
+        // Określona z góry ilość butli (na sztywno wpisane w bazie danych) -> poszczególne TextView dla każdej butli -> pobieranie wartości i zapisywanie nowych do bazy danych
+        //      Jeśli to rozwiązanie to: Jakie gazy? Jakie wartości dla nich? (aktualnie nazwa i wartość (napełnienie) w %)
+        //      ((AKTUALNIE WPROWADZONE))
+        // Swobodne dodawanie nowych butli -> pobieranie butli z bazy danych -> generowanie TextView dla każdej -> aktualizacja bazy - aktualizacja layoutu
+        //      Dosyć trudne rozwiązanie ale umożliwia dodawanie butli "w locie"
+        /*
+        LinearLayout linearLayout = new LinearLayout(this);
+        setContentView(linearLayout);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gaseslist);
-        gases.addValueEventListener(new ValueEventListener() {
+
+        final TextView textView_NO2 = findViewById(R.id.textView_NO2);
+        final TextView textView_NO2_value = findViewById(R.id.textView_NO2_value);
+        final Button button_NO2 = findViewById(R.id.button_NO2);
+
+
+        gases_ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Toast.makeText(GasesListActivity.this, "Key: " + snapshot.getKey() + "Value: " + snapshot.getValue() , Toast.LENGTH_SHORT).show();
-            /*
-            for (int i = 0; i < values.size(); i++){
-                String value = values.get(i).toString();
-                Toast.makeText(GasesListActivity.this, value , Toast.LENGTH_SHORT).show();
-            }
+                textView_NO2.setText(snapshot.child("0").child("name").getValue().toString());
+                textView_NO2_value.setText(snapshot.child("0").child("value").getValue().toString());
+                //TextView textView = new TextView(getBaseContext());
+                //textView.setText("Name: " + snapshot.child(String.valueOf(0)).child("name").getValue() + " Value: " + snapshot.child(String.valueOf(0)).child("value").getValue());
+                //linearLayout.addView(textView);
+                /*
+                for(int i = 0; i < snapshot.getChildrenCount(); i++){
 
-             */
 
+
+                    Toast.makeText(GasesListActivity.this, i + " Name: " + snapshot.child(String.valueOf(i)).child("name").getValue() + " Value: " + snapshot.child(String.valueOf(i)).child("amount").getValue() , Toast.LENGTH_SHORT).show();
+                }
+                */
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(GasesListActivity.this, "Failed to read value.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button_NO2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Odpalić okienko do: wpisania nowego stanu butli/suwak do ustalenia nowego stanu butli w %
+                startActivity(new Intent(GasesListActivity.this, ChangeValuePopUp.class));
             }
         });
 
