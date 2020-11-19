c7e45caafa253957e7a7342cff92888d7d6867f5
atat97 <a_tatusko@wp.pl>
2020-11-19 01:15:05 +0100

    Dynamically creating textviews for gases from the database

--- app/src/main/java/com/example/manager/GasesListActivity.java
+++ app/src/main/java/com/example/manager/GasesListActivity.java
@@ -3,16 +3,23 @@ package com.example.manager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -43,52 +50,51 @@ public class GasesListActivity extends AppCompatActivity {
         //      ((AKTUALNIE WPROWADZONE))
         // Swobodne dodawanie nowych butli -> pobieranie butli z bazy danych -> generowanie TextView dla każdej -> aktualizacja bazy - aktualizacja layoutu
         //      Dosyć trudne rozwiązanie ale umożliwia dodawanie butli "w locie"
-        /*
-        LinearLayout linearLayout = new LinearLayout(this);
-        setContentView(linearLayout);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        */
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gaseslist);
 
-        final TextView textView_NO2 = findViewById(R.id.textView_NO2);
-        final TextView textView_NO2_value = findViewById(R.id.textView_NO2_value);
-        final Button button_NO2 = findViewById(R.id.button_NO2);
+        LinearLayout gasesLinLayout = findViewById(R.id.gases_linear_layout);
+        TableLayout tableLayout = new TableLayout(this);
+        ProgressDialog progressDialog = new ProgressDialog(this);
 
 
         gases_ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                //Read
+
+                //Read gases from database
                 List<Gas> gasList = new ArrayList<>();
                 for(int i = 0; i < snapshot.getChildrenCount(); i++){
                     gasList.add(snapshot.child(String.valueOf(i)).getValue(Gas.class));
                 }
-                textView_NO2.setText(gasList.get(0).getName());
-                textView_NO2_value.setText(gasList.get(0).getValue());
 
-                //textView_NO2.setText(snapshot.child("0").child("name").getValue().toString() + ": ");
-                //textView_NO2_value.setText(snapshot.child("0").child("value").getValue().toString());
-
-                //TextView textView = new TextView(getBaseContext());
-                //textView.setText("Name: " + snapshot.child(String.valueOf(0)).child("name").getValue() + " Value: " + snapshot.child(String.valueOf(0)).child("value").getValue());
-                //linearLayout.addView(textView);
-                /*
-                for(int i = 0; i < snapshot.getChildrenCount(); i++){
+                //Display gases
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+                tableLayout.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                tableLayout.setShrinkAllColumns(true);
+                tableLayout.setStretchAllColumns(true);
 
 
 
-                    Toast.makeText(GasesListActivity.this, i + " Name: " + snapshot.child(String.valueOf(i)).child("name").getValue() + " Value: " + snapshot.child(String.valueOf(i)).child("amount").getValue() , Toast.LENGTH_SHORT).show();
+                for(int i = 0; i < gasList.size(); i++){
+                    displayGas(gasList, i, tableLayout);
                 }
-                */
+
+                gasesLinLayout.addView(tableLayout);
+
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(GasesListActivity.this, "Failed to read value.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(GasesListActivity.this, "Failed to read from database.", Toast.LENGTH_SHORT).show();
             }
         });
 
+        /*
         button_NO2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,7 +102,42 @@ public class GasesListActivity extends AppCompatActivity {
             }
         });
 
+
+         */
     }
 
+    protected void displayGas(List<Gas> gasList, int i, TableLayout tableLayout){
+        //First row: NAME & VALUE
+        TableRow rowNameAndValue = new TableRow(GasesListActivity.this);
+        TextView textName = new TextView(GasesListActivity.this);
+        textName.setText(gasList.get(i).getName());
+        rowNameAndValue.addView(textName);
+        TextView textValue = new TextView(GasesListActivity.this);
+        textValue.setText(gasList.get(i).getValue());
+        rowNameAndValue.addView(textValue);
+        tableLayout.addView(rowNameAndValue);
 
+        //Second row: USER
+        TableRow rowUser = new TableRow(GasesListActivity.this);
+        TextView textUser = new TextView(GasesListActivity.this);
+        textUser.setText(gasList.get(i).getUser());
+        rowUser.addView(textUser);
+        tableLayout.addView(rowUser);
+
+        //Third row: LOCATION
+        TableRow rowLocation = new TableRow(GasesListActivity.this);
+        TextView textLocation = new TextView(GasesListActivity.this);
+        textLocation.setText(gasList.get(i).getLocation());
+        rowLocation.addView(textLocation);
+        tableLayout.addView(rowLocation);
+
+        //Fourth row: ACQUISITION DATE
+        TableRow rowAcqDate = new TableRow(GasesListActivity.this);
+        TextView textAcqDate = new TextView(GasesListActivity.this);
+        textAcqDate.setText(gasList.get(i).getAcq_date());
+        rowAcqDate.addView(textAcqDate);
+        tableLayout.addView(rowAcqDate);
+
+        //Fifth row: EDIT BUTTON
+    }
 }
